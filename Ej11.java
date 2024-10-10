21aec1ca0269a52f73c24484656e94fc4f73687e
AlexPrzz <aleperper11@alu.edu.gva.es>
2024-10-10 10:36:33 +0200

    Update Ej11.java

--- Ej11.java
+++ Ej11.java
@@ -7,11 +7,36 @@ public class Ej11 {
 
         while (exit==false){
             try {
-                System.out.println("Introduzca un valor numérico: ");
+
+                System.out.println("Pulse 1 para recibir un número aleatorio");
+                System.out.println("Pulse 2 para parecer que has hecho algo, pero no");
+                System.out.println("Pulse 3 para hacer lo mismo que con el número 2");
+                int num=sc.nextInt();
+                switch (num){
+                    case 1:
+                        System.out.println(Math.random());
+                        exit=true;
+                        break;
+                    case 2:
+                        System.out.println("Gracias por pulsar 2, ahora estás en Matrix");
+                        exit=true;
+                        break;
+                    case 3:
+                        System.out.println("Has pulsado 3, gracias por no pulsar otro número");
+                        exit=true;
+                        break;
+                    default:
+                        System.out.println("Elige 1,2 o 3, gracias! ");
+                }
 
 
 
+            }catch (RuntimeException e){
+                System.out.println("Valores NÚMERICOS, no me pongas letritas");
+                sc.next();
             }
+            System.out.println("Gracias por gastar una pulsación de tu teclado");
+
 
         }
 
